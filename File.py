fe58656a73d35553ad5cf255747040fedfee72f8
egorrokita <egorrokita.china@gmail.com>
2023-09-14 10:11:40 +0300

    s

--- File.py
+++ File.py
@@ -2,19 +2,14 @@ a=int(input())
 b=int(input())
 
 for i in range(a,b+1):
+    div = []
     k = 0
-    for n in range(1,a+1):
-        if a % n ==0:
+    for n in range(1,i+1):
+        if i % n ==0:
             k = k+1
-    a=a+1
+            div.append(n)
     if k == 4:
-        div=[]
-        x = 0
         print(i)
-        for x in range(1,i+1):
-            if i % x ==0:
-                div.append(x)
-        x = x + 1
         print(div)
 
 
